84cb8d7b15ac19f499822155e5f6fc83ddc849b1
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2026-07-09 19:47:43 +0300

    refactor: comment out specific error handling logic in handleError function

--- src/auth/utils/index.ts
+++ src/auth/utils/index.ts
@@ -1,17 +1,17 @@
 import { AxiosError } from 'axios'
 
 export const handleError = (error: AxiosError) => {
-  if (error instanceof AxiosError) {
-    if (error.response?.status === 429) {
-      return Promise.reject(new Error('Too many attempts, please try again later'))
-    }
-    if (error.response?.status === 423) {
-      return Promise.reject(new Error('Account is locked, please contact support'))
-    }
-    if (error.response?.status === 401) {
-      return Promise.reject(new Error('Email or password is incorrect'))
-    }
-  }
+  // if (error instanceof AxiosError) {
+  //   if (error.response?.status === 429) {
+  //     return Promise.reject(new Error('Too many attempts, please try again later'))
+  //   }
+  //   if (error.response?.status === 423) {
+  //     return Promise.reject(new Error('Account is locked, please contact support'))
+  //   }
+  //   if (error.response?.status === 401) {
+  //     return Promise.reject(new Error('Email or password is incorrect'))
+  //   }
+  // }
 
-  return Promise.reject(new Error('An error occurred, please try again'))
+  return Promise.reject(new Error(error?.message || 'An error occurred, please try again'))
 }
